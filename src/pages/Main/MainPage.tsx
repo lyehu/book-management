e4156b7f31b5106f41c0f182a42a7912e03d4c65
Lijie Ye <ly232175@gmail.com>
2023-10-02 00:05:40 +0200

    add skeleton to main page

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -7,7 +7,7 @@ import Header from '../../components/atoms/Header';
 import Thumbnail from '../../components/atoms/Thumbnail';
 import ThumbnailList from '../../components/molecules/ThumbnailList';
 import { Link, useNavigate } from 'react-router-dom';
-import { Container } from '@mui/material';
+import { Container, Skeleton } from '@mui/material';
 import Button from '../../components/atoms/Button';
 import { ButtonContainer } from './MainPage.Styles';
 
@@ -39,13 +39,46 @@ export default function MainPage() {
         <Title>Books list</Title>
       </Header>
       <ThumbnailList>
-        {books?.map((eachBook: Book) => {
-          return (
-            <Link to={`/books/${eachBook.id}`} key={eachBook.id}>
-              <Thumbnail src={eachBook.coverUrl.value} alt={`${eachBook.title.value} details page`} />
-            </Link>
-          );
-        })}
+        {books.length ? (
+          books.map((eachBook: Book) => {
+            return (
+              <Link to={`/books/${eachBook.id}`} key={eachBook.id}>
+                <Thumbnail src={eachBook.coverUrl.value} alt={`${eachBook.title.value} details page`} />
+              </Link>
+            );
+          })
+        ) : (
+          <>
+            <Skeleton
+              height={240}
+              width={160}
+              sx={{
+                transform: 'scale(1)',
+              }}
+            />
+            <Skeleton
+              height={240}
+              width={160}
+              sx={{
+                transform: 'scale(1)',
+              }}
+            />
+            <Skeleton
+              height={240}
+              width={160}
+              sx={{
+                transform: 'scale(1)',
+              }}
+            />
+            <Skeleton
+              height={240}
+              width={160}
+              sx={{
+                transform: 'scale(1)',
+              }}
+            />
+          </>
+        )}
       </ThumbnailList>
       <ButtonContainer>
         <Button onClick={navigateToNewBook}>Add book</Button>
